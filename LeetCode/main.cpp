064d1cef31292c8b5ed55914c6997c6c8d42fb71
Curnic <Dorin>
2023-10-31 04:14:16 +0200

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Dorin Curnic on 31.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
